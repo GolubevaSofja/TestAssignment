d670ddc8fd0fe729616a25dbc6a41bf86ddee9f0
GolubevaSofja <133961497+GolubevaSofja@users.noreply.github.com>
2024-08-30 13:10:58 +0300

    fixed footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <footer class="mt-4 border-top pt-3">
     <div class="container text-center">
-        <p>Product List Application</p>
+        <p>Test assignment</p>
     </div>
 </footer>
 </html>
